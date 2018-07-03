a1954f9a88f28f38ffd916a002ad7922f55de3ae
dashwav <dashwav@gmail.com>
2018-07-03 00:31:13 -0700

    add levels command

---
+++ commands/levels.js
@@ -0,0 +1,26 @@
+const { Servers } = require('../dbObjects');
+
+function StringReplace(inString, serverName, url) {
+  if (inString.includes('%server_name%')) {
+    inString.replace(/%server_name%/g, `${serverName}`);
+  }
+  if (inString.includes('%url%')) {
+    inString.replace(/%url%/g, `${url}`);
+  }
+  return inString;
+}
+
+module.exports = {
+  name: 'levels',
+  description: 'Returns a url to the servers leaderboard',
+  async execute(message) {
+    const currentServer = await Servers.find({ where: { server_id: message.guild.id } });
+    if (!currentServer) {
+      message.channel.send('This server is not being tracked right now, if this is in error contact Dashwav');
+      return;
+    }
+    const newString = StringReplace(currentServer.levels_string, currentServer.server_name, `https://www.danbo.space/leaderboards/${currentServer.server_id}`);
+    message.channel.send(newString);
+  },
+};
+
