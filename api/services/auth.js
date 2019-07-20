a2c2e10c183906c7212fcac597c36a5cc5c04fb7
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2019-07-19 19:46:48 -0500

    auth module

---
+++ api/services/auth.js
@@ -0,0 +1,27 @@
+const { JWT } = require('@panva/jose');
+
+const config = require('../../config.json');
+const bottoken = config.token.split('.')[2];
+
+// JWT key construction
+
+exports.verify = function(req, res) {
+  console.log('auth:' + req.headers.authorization);
+  let p = null;
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
+    const jwt = JWT.decode(req.headers.authorization.split(' ')[1], { complete: true });
+    console.log(jwt.payload.token)
+
+    if (jwt.payload.token && jwt.payload.token != bottoken) {
+      res.statusMessage = 'Authentication failed. Incorrect Key.';
+      res.sendStatus(401);
+      return false;
+    } 
+    if (jwt.payload.token && jwt.payload.token === bottoken) {
+      return true;
+    }
+  }
+  res.statusMessage = 'Authentication failed. Key not found.';
+  res.sendStatus(401);
+  return false;
+};
